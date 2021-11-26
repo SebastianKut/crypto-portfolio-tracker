73e8d163e2506dba94fa04d2008c6a553e40bf56
sebastiankut <sebastian_kut@yahoo.co.uk>
2021-11-26 11:40:35 +0100

    initial commit

--- resources/js/Components/Components-dashboard/ChartBar.js
+++ resources/js/Components/Components-dashboard/ChartBar.js
@@ -1,37 +1,39 @@
-import { useEffect } from 'react';
-import Chart from 'chart.js';
-import Card from '@material-tailwind/react/Card';
-import CardHeader from '@material-tailwind/react/CardHeader';
-import CardBody from '@material-tailwind/react/CardBody';
+import { useEffect } from "react";
+import Chart from "chart.js";
+import Card from "@material-tailwind/react/Card";
+import CardHeader from "@material-tailwind/react/CardHeader";
+import CardBody from "@material-tailwind/react/CardBody";
 
 export default function ChartBar() {
     useEffect(() => {
         let config = {
-            type: 'bar',
+            type: "bar",
             data: {
                 labels: [
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                    'July',
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
                 ],
                 datasets: [
                     {
-                        label: new Date().getFullYear(),
-                        backgroundColor: '#03a9f4',
-                        borderColor: '#03a9f4',
+                        // label: new Date().getFullYear(),
+                        label: "Total Value",
+                        backgroundColor: "#03a9f4",
+                        borderColor: "#03a9f4",
                         data: [30, 78, 56, 34, 100, 45, 13],
                         fill: false,
                         barThickness: 8,
                     },
                     {
-                        label: new Date().getFullYear() - 1,
+                        // label: new Date().getFullYear() - 1,
+                        label: "Cost",
                         fill: false,
-                        backgroundColor: '#f44336',
-                        borderColor: '#f44336',
+                        backgroundColor: "#f44336",
+                        borderColor: "#f44336",
                         data: [27, 68, 86, 74, 10, 4, 87],
                         barThickness: 8,
                     },
@@ -42,22 +44,22 @@ export default function ChartBar() {
                 responsive: true,
                 title: {
                     display: false,
-                    text: 'Orders Chart',
+                    text: "Orders Chart",
                 },
                 tooltips: {
-                    mode: 'index',
+                    mode: "index",
                     intersect: false,
                 },
                 hover: {
-                    mode: 'nearest',
+                    mode: "nearest",
                     intersect: true,
                 },
                 legend: {
                     labels: {
-                        fontColor: 'rgba(17,17,17,.7)',
+                        fontColor: "rgba(17,17,17,.7)",
                     },
-                    align: 'end',
-                    position: 'bottom',
+                    align: "end",
+                    position: "bottom",
                 },
                 scales: {
                     xAxes: [
@@ -65,13 +67,13 @@ export default function ChartBar() {
                             display: false,
                             scaleLabel: {
                                 display: true,
-                                labelString: 'Month',
+                                labelString: "Month",
                             },
                             gridLines: {
                                 borderDash: [2],
                                 borderDashOffset: [2],
-                                color: 'rgba(33, 37, 41, 0.3)',
-                                zeroLineColor: 'rgba(33, 37, 41, 0.3)',
+                                color: "rgba(33, 37, 41, 0.3)",
+                                zeroLineColor: "rgba(33, 37, 41, 0.3)",
                                 zeroLineBorderDash: [2],
                                 zeroLineBorderDashOffset: [2],
                             },
@@ -82,14 +84,14 @@ export default function ChartBar() {
                             display: true,
                             scaleLabel: {
                                 display: false,
-                                labelString: 'Value',
+                                labelString: "Value",
                             },
                             gridLines: {
                                 borderDash: [2],
                                 drawBorder: false,
                                 borderDashOffset: [2],
-                                color: 'rgba(33, 37, 41, 0.2)',
-                                zeroLineColor: 'rgba(33, 37, 41, 0.15)',
+                                color: "rgba(33, 37, 41, 0.2)",
+                                zeroLineColor: "rgba(33, 37, 41, 0.15)",
                                 zeroLineBorderDash: [2],
                                 zeroLineBorderDashOffset: [2],
                             },
@@ -98,7 +100,7 @@ export default function ChartBar() {
                 },
             },
         };
-        let ctx = document.getElementById('bar-chart').getContext('2d');
+        let ctx = document.getElementById("bar-chart").getContext("2d");
         window.myBar = new Chart(ctx, config);
     }, []);
     return (
@@ -107,7 +109,7 @@ export default function ChartBar() {
                 <h6 className="uppercase text-gray-200 text-xs font-medium">
                     Overview
                 </h6>
-                <h2 className="text-white text-2xl">Sales value</h2>
+                <h2 className="text-white text-2xl">Portfolio value</h2>
             </CardHeader>
             <CardBody>
                 <div className="relative h-96">
